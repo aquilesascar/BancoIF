519a13b80fa1fa34a707a111840603525274f1cc
aquilesascar <ascaraquiles@gmail.com>
2024-12-20 21:54:26 -0300

    função aquiles

---
+++ src/bancoimobiliario/Aquiles.java
@@ -0,0 +1,13 @@
+package bancoimobiliario;
+
+public class Aquiles {
+String aquiles;
+String user;
+    public boolean bonito() {
+        if (user == aquiles){
+            System.out.println("Ele é bonito!");
+        }else{
+            System.out.println("credoooo!");
+        }
+    }
+}
